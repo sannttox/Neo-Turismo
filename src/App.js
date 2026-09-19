a43e959911ab61bbb98ae74535f8f39b667e3a2b
robison.santos <robison.santos@mundiware.com>
2026-09-19 18:10:34 -0300

    Ajudando a construir os componentes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,102 @@
-import React from 'react';
+import Header from './header.js';
+
 import './App.css';
 
 
-
 function App() {
-
-
-
   return (
     <div className="container">
       {/* Cabeçalho do Site */}
-      <header className="header">
+      {/* <header className="header">
         <h1>Pontos Turísticos de Neópolis - SE</h1>
         <p>Conheça as belezas e tradições da Capital do Frevo Sergipano</p>
-      </header>
+      </header> */}
+
+      <Header />
+
+
 
       {/* Conteúdo Principal */}
       <main className="main-content">
+
         <section className="cards-grid">
-          {/*  
-            />
-          ))} */}
+
           <div className="card">
-            {/* <img src alt={`Foto de ${nome}`} className="card-img" /> */}
+            <img src="/imagem/aldeia.jpg" alt="Aldeia" className="card-img" />
             <div className="card-body">
               <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
               <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
+            </div>
+          </div>
 
-              {/*  <span className="card-location">📍 {localizacao}</span> */}
+          <div className="card">
+            <img src="/imagem/aracare.jpg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
             </div>
-            </div>
-            <div className="card">
-              {/* <img src alt={`Foto de ${nome}`} className="card-img" /> */}
-              <div className="card-body">
-                <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
-                <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+          </div>
 
-                {/*  <span className="card-location">📍 {localizacao}</span> */}
-              </div>
+          <div className="card">
+            <img src="/imagem/balsas.jpeg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
             </div>
+          </div>
+
+          <div className="card">
+            <img src="/imagem/monsenhor.jpg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/imagem/oportinho.jpg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/imagem/passagem.jpg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img src="/imagem/sao-sebastiao.jpeg" alt="Aldeia" className="card-img" />
+            <div className="card-body">
+              <h2>Igreja de Nossa Senhora do Rosário Século XVII</h2>
+              <p>É considerada a segunda igreja mais antiga de Sergipe. Teve uma origem peculiar: funcionou originalmente como um forte militar construído por Maurício de Nassau durante o domínio holandês na região. Após a retomada dos portugueses, o quartel-general foi convertido em templo religioso católico pela Confraria de São Benedito dos Homens Pretos. Curiosamente, ela fica de costas para o Rio São Francisco</p>
+              <p className="card-location">📍localizacao</p>
+            </div>
+          </div>
+
+        </section>
+
+        <section className='form'>
+          <form action="">
+            <h2>Formulário</h2>
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome"></input>
+
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email"></input>
+
+            <button type="submit">Enviar</button>
+          </form>
         </section>
 
       </main>
@@ -47,6 +105,8 @@ function App() {
       <footer className="footer">
         <p>© 2026 - Projeto de Programação Web I | Neópolis - Sergipe</p>
       </footer>
+
+
     </div>
   );
 }
